84b307125513da74b42920c59eeb85d29fe09316
Tommy Reynolds <u1309995@utah.edu>
2024-04-10 14:33:56 -0600

    misc

--- SuburbanOutfitters/customer/models.py
+++ SuburbanOutfitters/customer/models.py
@@ -1,6 +1,46 @@
 from django.db import models
 
-# Create your models here.
-#Alex Test Push
-#test
-#tommy test push2
+class Product(models.Model):
+    productName = models.CharField(max_length=200)
+    productType = models.CharField(max_length=200)
+    
+
+
+
+
+
+
+
+
+
+
+# class tShirt(models.Model):
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     color = models.CharField(max_length=100)
+#     size = models.CharField(max_length=100)
+#     brand = models.CharField(max_length=100)
+
+# class Pants(models.Model):
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     color = models.CharField(max_length=100)
+#     size = models.CharField(max_length=100)
+#     brand = models.CharField(max_length=100)
+    
+# class Shorts(models.Model):
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     color = models.CharField(max_length=100)
+#     size = models.CharField(max_length=100)
+#     brand = models.CharField(max_length=100)
+
+# class sweatShirt(models.Model):
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     color = models.CharField(max_length=100)
+#     size = models.CharField(max_length=100)
+#     brand = models.CharField(max_length=100)
+
+# class Hats(models.Model):
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     color = models.CharField(max_length=100)
+#     size = models.CharField(max_length=100)
+#     brand = models.CharField(max_length=100)
+
