848c0c9d2d01465c9832f6aff43885367a46326b
Tommy Reynolds <u1309995@utah.edu>
2024-04-10 15:03:12 -0600

    misc

--- SuburbanOutfitters/customer/views.py
+++ SuburbanOutfitters/customer/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Product
 
-# Create your views here.
+class ProductListView(View):
+    def get(self, request):
+        products = Product.objects.all()
+        context = {
+            'product': products
+        }
+        return render(request, 'product_list.html', context=context)
