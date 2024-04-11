17a54b0bc7003a0531abd86a4cfc92461449aef7
Alex <u1385005@utah.edu>
2024-04-10 23:12:33 -0600

    Product Crud ops

--- SuburbanOutfitters/customer/urls.py
+++ SuburbanOutfitters/customer/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import ProductListView, CustomerList
+from .views import ProductListView, CustomerList,ProductAdd
+from . import views
 
 urlpatterns = [
     path('shop/', ProductListView.as_view(), name="product_list"),
     path('customer/',CustomerList.as_view(), name="customer_list"),
+    path('product_add/', views.ProductAdd.as_view(), name = "product_add"),
+    path('product_details/<int:product_id>/', views.ProductDetails.as_view(), name='product_details'),
+    path('product_update/<int:product_id>/', views.ProductUpdate.as_view(), name='product_update'),
+    path('product_delete/<int:product_id>/', views.ProductDelete.as_view(), name='product_delete'),
 ]
--- SuburbanOutfitters/customer/views.py
+++ SuburbanOutfitters/customer/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Product,Customer
+from .forms import ProductForm
+from django.urls import reverse
 
 class ProductListView(View):
     def get(self, request):
@@ -14,3 +16,85 @@ class CustomerList(View):
     def get(self,request):
         customers = Customer.objects.all()
         return render(request = request,template_name = 'customer_list.html',context = {'customers':customers})
+    
+
+class ProductAdd(View):
+
+    def get(self,request):
+        form = ProductForm()
+        return render(request = request,template_name = 'product_add.html',context = {'form':form})
+    
+    def post(self,request):
+        form = ProductForm(request.POST)
+
+        if form.is_valid():
+            product = form.save()
+        
+        return render(request = request,template_name = 'product_add.html',context = {'product':product,'form':form})
+    
+
+class ProductDetails(View):
+
+    def get(self,request,product_id=None):
+
+        if product_id:
+            product = Product.objects.get(pk=product_id)
+        else:
+            product = Product()
+
+        form = ProductForm(instance=product)
+
+        for field in form.fields:
+            form.fields[field].widget.attrs['disabled'] = True
+
+        return render(request = request,template_name = 'product_details.html',context = {'product':product,'form':form})
+    
+    def post(self,request,product_id=None):
+        return redirect(reverse("product_list"))
+    
+   
+class ProductUpdate(View):
+
+    def get(self,request,product_id):
+
+        product = Product.objects.get(pk=product_id)
+        form = ProductForm(instance=product)
+
+        return render(request = request,template_name = 'product_update.html',context = {'product':product,'form':form})
+    
+    def post(self,request,product_id):
+
+        product = Product.objects.get(pk=product_id)
+        form = ProductForm(request.POST,instance=product)
+
+        if form.is_valid():
+            product = form.save()
+        
+        return render(request = request,template_name = 'product_update.html',context = {'product':product,'form':form})
+
+
+class ProductDelete(View):
+
+    def get(self,request,product_id=None):
+
+        if product_id:
+            product = Product.objects.get(pk=product_id)
+        else:
+            product = Product()
+
+        form = ProductForm(instance=product)
+
+        for field in form.fields:
+            form.fields[field].widget.attrs['disabled'] = True
+
+        return render(request = request,template_name = 'product_delete.html',context = {'product':product,'form':form})
+    
+    def post(self,request,product_id=None):
+
+        product = Product.objects.get(pk=product_id)
+
+        form = ProductForm(request.POST,instance=product)
+
+        product.delete()
+
+        return redirect(reverse("product_list"))
